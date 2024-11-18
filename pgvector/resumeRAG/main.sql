90c61311888ed30a3c6055931ee9ad128d5aba7e
Jun Yeow <junyeow27@gmail.com>
2024-11-18 08:57:23 +0800

    Trying out RAG system from scratch for personal project

--- pgvector/resumeRAG/main.sql
+++ pgvector/resumeRAG/main.sql
@@ -12,4 +12,29 @@ FROM langchain_pg_embedding;
 
 -- Removing the values within the langchain_pg_embedding
 TRUNCATE TABLE langchain_pg_embedding;
+
+-- Connecting the data together to make sure the data is uniquely
+SELECT 
+	embed.document,
+	collect.name,
+	collect.cmetadata
+FROM langchain_pg_embedding AS embed
+LEFT JOIN langchain_pg_collection AS collect
+			 ON embed.collection_id = collect.uuid;
 ---------------------------------------------------------------------------------------------------------
+
+-- ALTER TABLE langchain_pg_embedding
+-- ADD COLUMN IF NOT EXISTS created_datetime TIMESTAMP;
+
+-- UPDATE langchain_pg_embedding
+-- SET created_datetime = NOW();
+
+---------------------------------------------------------------------------------------------------------
+
+-- Change the connection to the other database
+
+SELECT document
+FROM langchain_pg_embedding
+LIMIT 1;
+
+-- TRUNCATE TABLE langchain_pg_embedding;
